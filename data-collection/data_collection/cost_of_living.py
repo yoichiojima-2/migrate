a7dbc6206a2770fe7017311ac36c3b9cc89f5474
Yoichi Ojima <yoichiojima@gmail.com>
2024-11-24 17:01:45 +0900

    [change] add country to cost-of-living-data

--- data-collection/data_collection/cost_of_living.py
+++ data-collection/data_collection/cost_of_living.py
@@ -13,27 +13,48 @@ class CostOfLivingTask(Task):
     @staticmethod
     def _scrap(city: str, currency: str = "JPY") -> pd.DataFrame:
         url = f"https://www.numbeo.com/cost-of-living/in/{city}?displayCurrency={currency}"
+        print(f"[CostOfLivingTask._scrap] url: {url}")
         # fmt: off
         headers = {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.5993.122 Safari/537.36"}
         # fmt: on
         response = requests.get(url, headers=headers)
         soup = BeautifulSoup(response.text, "html.parser")
-
-        table = soup.find("table", {"class": "data_wide_table"})
-        rows = table.find_all("tr")
+        rows = soup.find("table", {"class": "data_wide_table"}).find_all("tr")
 
         cost = []
         for row in rows:
             cells = row.find_all("td")
             if len(cells) > 1:
                 cost.append(
-                    {"item": cells[0].text.strip(), "cost": float(cells[1].text.strip().replace("\xa0¥", "").replace(",", ""))}
+                    {
+                        "item": cells[0].text.strip(),
+                        "country": soup.find_all("a", class_="breadcrumb_link")[1].text,
+                        "city": city,
+                        "currency": currency,
+                        "cost": float(cells[1].text.strip().replace("\xa0¥", "").replace(",", "")),
+                    }
                 )
 
         return pd.DataFrame(cost)
 
     def extract(self) -> pd.DataFrame:
-        cities = ["Tokyo", "London", "New-York", "Brisbane", "Bristol", "Manchester"]
+        # fmt: off
+        cities = [
+            "Tokyo",
+            "London",
+            "New-York",
+            "Brisbane",
+            "Bristol",
+            "Manchester",
+            "Mumbai",
+            "Bangkok",
+            "Kuala-Lumpur",
+            "Singapore",
+            "Taipei",
+            "Shanghai",
+            "Hong-Kong"
+        ]
+        # fmt: of
         return pd.concat([self._scrap(city) for city in cities])
 
     def transform(self, df: pd.DataFrame) -> pd.DataFrame:
@@ -41,3 +62,10 @@ class CostOfLivingTask(Task):
 
     def load(self, df: pd.DataFrame) -> pd.DataFrame:
         df.to_json(Path(os.getenv("DATA_DIR")) / "cost_of_living.json", orient="records")
+
+
+if __name__ == "__main__":
+    task = CostOfLivingTask()
+    df = task.extract()
+    df = task.transform(df)
+    task.load(df)
