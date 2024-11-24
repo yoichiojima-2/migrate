68df65112e0cd201f9346a5ba2a125ae59c8b055
Yoichi Ojima <yoichiojima@gmail.com>
2024-11-24 22:09:53 +0900

    show less rows on test

--- data-collection/tests/test_living_of_cost.py
+++ data-collection/tests/test_living_of_cost.py
@@ -8,5 +8,5 @@ from data_collection.cost_of_living import CostOfLivingTask
 def test_cost_of_living():
     CostOfLivingTask().run()
     output = Path(os.getenv("DATA_DIR")) / "cost_of_living.json"
-    pprint(json.load(output.open()))
+    pprint(json.load(output.open())[:5])
     assert output.exists()
