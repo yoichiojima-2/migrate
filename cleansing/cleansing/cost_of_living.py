722354f1ac80f0724e1d38f6c7e2d75c18c58df4
Yoichi Ojima <yoichiojima@gmail.com>
2024-11-25 21:36:45 +0900

    [fix] cleansing:cost_of_living

--- cleansing/cleansing/cost_of_living.py
+++ cleansing/cleansing/cost_of_living.py
@@ -5,7 +5,7 @@ import pandas as pd
 
 def cleanse():
     df = pd.read_json(f"{os.getenv('DATA_DIR')}/cost_of_living.json")
-    df = df.pivot(index="city", columns="item", values="cost")
+    df = df.pivot(index="city", columns="item", values="cost").reset_index()
     df.to_json(
         Path(os.getenv("DATA_DIR")) / "cost_of_living_cleansed.json",
         orient="records",
