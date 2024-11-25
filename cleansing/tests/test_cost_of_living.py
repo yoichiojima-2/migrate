65eb5241fdc54f718ae0e89d057c7ee791b12402
Yoichi Ojima <yoichiojima@gmail.com>
2024-11-25 21:56:35 +0900

    fix failing test

--- cleansing/tests/test_cost_of_living.py
+++ cleansing/tests/test_cost_of_living.py
@@ -2,11 +2,11 @@ import os
 from pathlib import Path
 from pprint import pprint
 import json
-from cleansing.cost_of_living import cleanse
+from cleansing import cost_of_living
 
 
 def test_cost_of_living():
-    cleanse()
+    cost_of_living.cleanse()
     output = Path(os.getenv("DATA_DIR")) / "cost_of_living_cleansed.json"
     pprint(json.load(output.open())[:5])
     assert output.exists()
