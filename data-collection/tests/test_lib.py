86c5e1df03b4a7727da8e8ab17e7239c1a2387ee
Yoichi Ojima <yoichiojima@gmail.com>
2024-11-25 21:59:52 +0900

    misc change

--- data-collection/tests/test_lib.py
+++ data-collection/tests/test_lib.py
@@ -8,5 +8,5 @@ from data_collection.task import Task
 def run_and_check_output(task_class: Task, output_name: str):
     task_class().run()
     output = Path(os.getenv("DATA_DIR")) / output_name
-    pprint(json.load(output.open())[:5])
+    pprint(json.load(output.open())[:2])
     assert output.exists()
