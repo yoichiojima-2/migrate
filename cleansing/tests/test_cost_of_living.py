af6188ea3dbcb45e2707e2435df22e54673ca6cc
Yoichi Ojima <yoichiojima@gmail.com>
2024-11-25 21:35:01 +0900

    [fix] fix .gitignore

---
+++ cleansing/tests/test_cost_of_living.py
@@ -0,0 +1,10 @@
+import os
+from pathlib import Path
+from pprint import pprint
+import json
+
+
+def test_cost_of_living():
+    output = Path(os.getenv("DATA_DIR")) / "cost_of_living_cleansed.json"
+    pprint(json.load(output.open())[:5])
+    assert output.exists()
