c6c580dfa4dcb61758c1af55939cbd64d2d52dee
yo <145314115+yoichiojima-2@users.noreply.github.com>
2024-11-28 02:01:10 +0900

    Update test_cost_of_living.py

--- collection/tests/test_cost_of_living.py
+++ collection/tests/test_cost_of_living.py
@@ -8,6 +8,3 @@ from tests.test_lib import run_and_check_output
 
 def test_cost_of_living():
     run_and_check_output(CostOfLivingTask, CostOfLivingTask.output_name)
-    output = Path(os.getenv("APP_ROOT")) / "data/cost_of_living_cleansed.json"
-    pprint(json.load(output.open())[:5])
-    assert output.exists()
