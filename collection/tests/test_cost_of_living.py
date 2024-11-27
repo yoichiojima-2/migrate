a98536eea130be77c838eba9d7451ced8f427495
Yoichi Ojima <yoichiojima@gmail.com>
2024-11-28 01:59:11 +0900

    update

--- collection/tests/test_cost_of_living.py
+++ collection/tests/test_cost_of_living.py
@@ -2,11 +2,12 @@ import os
 from pathlib import Path
 from pprint import pprint
 import json
-from cleansing import cost_of_living
+from collection.cost_of_living import CostOfLivingTask
+from tests.test_lib import run_and_check_output
 
 
 def test_cost_of_living():
-    cost_of_living.cleanse()
-    output = Path(os.getenv("DATA_DIR")) / "cost_of_living_cleansed.json"
+    run_and_check_output(CostOfLivingTask, CostOfLivingTask.output_name)
+    output = Path(os.getenv("APP_ROOT")) / "data/cost_of_living_cleansed.json"
     pprint(json.load(output.open())[:5])
     assert output.exists()
