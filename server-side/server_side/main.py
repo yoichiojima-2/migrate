4a24e9a8fd8441f55de85b3f29e67e079e3d99d5
Yoichi Ojima <yoichiojima@gmail.com>
2024-11-30 21:49:55 +0900

    [change] cor: allow all origin

--- server-side/server_side/main.py
+++ server-side/server_side/main.py
@@ -1,11 +1,19 @@
 import os
 from pathlib import Path
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import pandas as pd
 
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], # todo: replace with the actual frontend URL
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/summary")
 def summary(city: str):
