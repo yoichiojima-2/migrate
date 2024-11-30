ca8677ad2f886adde694a4f381ffdfc81e582224
Yoichi Ojima <yoichiojima@gmail.com>
2024-11-30 22:45:29 +0900

    update

--- client-side/src/App.tsx
+++ client-side/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 
 interface CityData {
@@ -12,15 +12,15 @@ interface CityData {
 }
 
 function App() {
-  const [data, setData] = useState<CityData[]>([]);
+  // fetch server
+  const [summary, setSummary] = useState<CityData[]>([]);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const res = await fetch("http://127.0.0.1:8000/summary?city=tokyo");
         const json = await res.json();
-        setData(json);
-        console.log("App.tsx::fetchData::json", json.slice(0, 5));
+        setSummary(json);
       } catch (error: any) {
         console.log(error.message);
       }
@@ -28,10 +28,15 @@ function App() {
     fetchData();
   }, []);
 
+  // log data
+  for (let i = 0; i < summary.length; i++) {
+    console.log(summary[i]);
+  }
+
   return (
     <>
       <div>
-        <h1>sign-to-migrate</h1>
+        <h1>migrate</h1>
       </div>
     </>
   );
