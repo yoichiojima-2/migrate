98dff07ef4abd08a2f7706e1d2d36de95d80c0e7
Yoichi Ojima <yoichiojima@gmail.com>
2024-11-30 21:50:26 +0900

    update client-side app

--- client-side/src/App.tsx
+++ client-side/src/App.tsx
@@ -1,14 +1,45 @@
-import './App.css'
+import React, { useEffect, useState } from "react";
+import "./App.css";
+
+interface CityData {
+  city: string;
+  country: string;
+  feature: string;
+  value: number;
+  value_in_current_city: number;
+  diff_amount: number;
+  diff_rate: number;
+}
 
 function App() {
+  const [data, setData] = useState<CityData[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await fetch("http://127.0.0.1:8000/summary?city=tokyo");
+        const json = await res.json();
+        setData(json);
+        console.log("App.tsx::fetchData::json", json);
+      } catch (error: any) {
+        console.log(error.message);
+      }
+    };
+    fetchData();
+  }, []);
 
   return (
     <>
       <div>
-        client-side
+        <h1>client-side</h1>
+        <h2>client-side</h2>
+        <h3>client-side</h3>
+        <h4>client-side</h4>
+        <h5>client-side</h5>
+        <p>client-side</p>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
