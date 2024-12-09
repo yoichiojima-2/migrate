b624105bcb226e8b432c09f3c3a86dc0937d5ca4
Yoichi Ojima <yoichiojima@gmail.com>
2024-12-10 00:35:54 +0900

    merge cleansing to collection

--- collection/tests/test_labour.py
+++ collection/tests/test_labour.py
@@ -1,46 +1,46 @@
-from collection import labor
+from collection import labour
 from tests.test_lib import run_and_check_output
 
 
 def test_working_poverty_rate():
-    run_and_check_output(labor.WorkingPovertyRate, "working_poverty_rate.json")
+    run_and_check_output(labour.WorkingPovertyRate, "raw/working_poverty_rate.json")
 
 
 def test_social_protection():
-    run_and_check_output(labor.SocialProtection, "social_protection.json")
+    run_and_check_output(labour.SocialProtection, "raw/social_protection.json")
 
 
 def test_women_in_senior_and_middle_position():
-    run_and_check_output(labor.WomenInSeniorAndMiddlePosition, "women_in_senior_and_middle_position.json")
+    run_and_check_output(labour.WomenInSeniorAndMiddlePosition, "raw/women_in_senior_and_middle_position.json")
 
 
 def test_women_in_managerial_position():
-    run_and_check_output(labor.WomenInManagerialPosition, "women_in_managerial_position.json")
+    run_and_check_output(labour.WomenInManagerialPosition, "raw/women_in_managerial_position.json")
 
 
 def test_annual_growth_rate_per_worker():
-    run_and_check_output(labor.AnnualGrowthRatePerWorker, "annual_growth_rate_per_worker.json")
+    run_and_check_output(labour.AnnualGrowthRatePerWorker, "raw/annual_growth_rate_per_worker.json")
 
 
 def test_informal_employment():
-    run_and_check_output(labor.InformalEmployment, "informal_employment.json")
+    run_and_check_output(labour.InformalEmployment, "raw/informal_employment.json")
 
 
 def test_average_hourly_earnings():
-    run_and_check_output(labor.AverageHourlyEarnings, "average_hourly_earnings.json")
+    run_and_check_output(labour.AverageHourlyEarnings, "raw/average_hourly_earnings.json")
 
 
 def test_unemployment_rate():
-    run_and_check_output(labor.UnemploymentRate, "unemployment_rate.json")
+    run_and_check_output(labour.UnemploymentRate, "raw/unemployment_rate.json")
 
 
 def test_unemployment_rate_disability():
-    run_and_check_output(labor.UnemploymentRateDisability, "unemployment_rate_disability.json")
+    run_and_check_output(labour.UnemploymentRateDisability, "raw/unemployment_rate_disability.json")
 
 
 def test_youth_neet_proportion():
-    run_and_check_output(labor.YouthNeetProportion, "youth_neet_proportion.json")
+    run_and_check_output(labour.YouthNeetProportion, "raw/youth_neet_proportion.json")
 
 
 def test_labour_rights():
-    run_and_check_output(labor.LabourRights, "labour_rights.json")
+    run_and_check_output(labour.LabourRights, "raw/labour_rights.json")
