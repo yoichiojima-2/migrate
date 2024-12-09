64ffae132ec715965dc941c09eb103ab17a92afe
Yoichi Ojima <yoichiojima@gmail.com>
2024-12-09 23:32:28 +0900

    tidy

--- utils/utils/utils.py
+++ utils/utils/utils.py
@@ -1,4 +1,3 @@
-import os
 from pathlib import Path
 
 import pandas as pd
@@ -14,24 +13,25 @@ def get_config() -> dict[str, any]:
     return yaml.safe_load((get_root() / "config.yml").open())
 
 
-def _get_data_dir() -> Path:
+def get_data_dir() -> Path:
     return get_root() / "data"
 
 
-def json_to_df(path: str) -> None:
-    path = _get_data_dir() / path
+def read_json(path: str) -> None:
+    output_path = get_data_dir() / path
 
-    if not path.suffix == ".json":
+    if not output_path.suffix == ".json":
         raise ValueError(f"output path must be a json: {path}")
 
-    return pd.read_json(path)
+    return pd.read_json(output_path)
 
 
 def df_to_json(df: pd.DataFrame, path: str) -> None:
-    path = _get_data_dir() / path
+    output_path = get_data_dir() / path
 
-    if not path.suffix == ".json":
+    if not output_path.suffix == ".json":
         raise ValueError(f"output path must be a json: {path}")
 
-    df.to_json(path, orient="records", index=False, indent=2)
-    print(f"[df_to_json] saved: {path}")
+    output_path.parent.mkdir(parents=True, exist_ok=True)
+    df.to_json(output_path, orient="records", index=False, indent=2)
+    print(f"[df_to_json] saved: {output_path}")
