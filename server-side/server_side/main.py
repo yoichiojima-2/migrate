d9a7a7d2f04cb55bcd76f0b90b97d226faf76272
Yoichi Ojima <yoichiojima@gmail.com>
2024-12-10 23:38:51 +0900

    add hapiness to api

--- server-side/server_side/main.py
+++ server-side/server_side/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from utils.utils import get_config
+import pandas as pd
+from utils.utils import get_config, get_data_dir
 
 
 app = FastAPI()
@@ -17,3 +18,51 @@ app.add_middleware(
 @app.get("/cities")
 def cities() -> list[str]:
     return get_config().get("cities")
+
+
+@app.get("/happiness")
+def happiness(country: str):
+    df = pd.read_json(get_data_dir() / "raw/happiness.json")
+
+    df = df[df["year"] == 2017].drop(columns="year")
+    df["country"] = df["country"].str.lower()
+    df["feature"] = df["feature"].str.lower()
+
+    # fmt: off
+    needle_df = (
+        df[df["country"] == country]
+        .rename(columns={"value": "needle_value"})
+        .drop(columns="country")
+    )
+
+    haystack_df = (
+        df[df["country"] != country]
+        .rename(columns={"value": "haystack_value"})
+    )
+    # fmt: on
+
+    merged_df = haystack_df.merge(needle_df, on=["feature"], how="left")
+    merged_df["diff_amount"] = merged_df["haystack_value"] - merged_df["needle_value"]
+
+    # fmt: off
+    merged_df["diff_rate"] = (
+        merged_df
+        .apply(lambda x: x["haystack_value"] / x["needle_value"] if x["needle_value"] else 0, axis=1)
+    )
+    # fmt: on
+    result = {}
+
+    for _, row in merged_df.iterrows():
+        country = row["country"]
+        feature = row["feature"]
+        if country not in result:
+            result[country] = {}
+
+        result[country][feature] = {
+            "haystack_value": row["haystack_value"],
+            "needle_value": row["needle_value"],
+            "diff_amount": row["diff_amount"],
+            "diff_rate": row["diff_rate"]
+        }
+
+    return result
