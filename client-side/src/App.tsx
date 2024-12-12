ee2e83cf4a2e2c6a2f2ee9c449baaa07b7f99d80
Yoichi Ojima <yoichiojima@gmail.com>
2024-12-12 20:50:19 +0900

    fetch country and happiness in client-side

--- client-side/src/App.tsx
+++ client-side/src/App.tsx
@@ -8,6 +8,8 @@ function App() {
   // fetch server
   const [cities, setCities] = useState<string[]>([]);
   const [currentCity, setCurrentCity] = useState<string>("tokyo");
+  const [country, setCountry] = useState("");
+  const [happiness, setHappiness] = useState({});
 
   useEffect(() => {
     const fetchData = async () => {
@@ -15,7 +17,7 @@ function App() {
         const res = await fetch(`${api_endpoint}/cities`);
         const json = await res.json();
         setCities(json);
-        console.log("cities updated");
+        console.log("cities updated.");
         console.log(json);
       } catch (error: any) {
         console.log(error.message);
@@ -23,6 +25,37 @@ function App() {
     };
     fetchData();
   }, []);
+
+  
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await fetch(`${api_endpoint}/country?city=${currentCity}`);
+        const json = await res.json();
+        console.log(json);
+        setCountry(json);
+        console.log(`country name fetched: ${country}`)
+      } catch (error: any) {
+        console.log(error.message);
+      }
+    };
+    fetchData();
+  }, [currentCity]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await fetch(`${api_endpoint}/happiness?country=${country}`);
+        const json = await res.json();
+        setHappiness(json);
+        console.log("happiness fetched:")
+        console.log(json);
+      } catch (error: any) {
+        console.log(error.message);
+      }
+    };
+    fetchData();
+  }, [currentCity]);
 
 
   return (
