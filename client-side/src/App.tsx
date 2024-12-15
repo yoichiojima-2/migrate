49f423a7f197b4b43fde5480164011e251c27210
Yoichi Ojima <yoichiojima@gmail.com>
2024-12-15 23:55:54 +0900

    update client side

--- client-side/src/App.tsx
+++ client-side/src/App.tsx
@@ -1,51 +1,21 @@
 import { useEffect, useState } from "react";
+import Happiness from "./pages/happiness";
+import { API_URL } from "./constants/api";
 import "./App.css";
 
-const api_endpoint: string = "http://127.0.0.1:8000";
 
-const Card = () => (
-  <div className="max-w-sm rounded overflow-hidden shadow-lg bg-white">
-    <div className="px-6 py-4">
-      <h2 className="font-bold text-xl mb-2">Card Title</h2>
-      <p className="text-gray-700 text-base">
-        This is a simple card description. It gives a brief overview of the
-        card's content.
-      </p>
-    </div>
-    <div className="px-6 pt-4 pb-2">
-      <button className="bg-blue-500 text-white font-bold py-2 px-4 rounded hover:bg-blue-700">
-        Action
-      </button>
-    </div>
-  </div>
-);
 
-function App() {
-  const [cities, setCities] = useState<string[]>([]);
+function App(): JSX.Element {
   const [currentCity, setCurrentCity] = useState<string>("tokyo");
-  const [country, setCountry] = useState("japan");
-  const [happiness, setHappiness] = useState([]);
+  const [country, setCountry] = useState<string>("japan");
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const res = await fetch(`${api_endpoint}/cities`);
-        const json = await res.json();
-        setCities(json);
-        console.log(cities);
-      } catch (error: any) {
-        console.log(error.message);
-      }
-    };
-    fetchData();
-  }, []);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const res = await fetch(`${api_endpoint}/country?city=${currentCity}`);
+        const res = await fetch(`${API_URL}/country?city=${currentCity}`);
         const json = await res.json();
         setCountry(json);
+        console.log(country);
       } catch (error: any) {
         console.log(error.message);
       }
@@ -53,25 +23,9 @@ function App() {
     fetchData();
   }, [currentCity]);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const res = await fetch(`${api_endpoint}/happiness?country=${country}`);
-        const json = await res.json();
-        const data = json["data"];
-        setHappiness(data);
-        console.log(happiness);
-      } catch (error: any) {
-        console.log(error.message);
-      }
-    };
-    fetchData();
-  }, [country]);
-
   return (
-    <div className="max-w-sm rounded overflow-hidden shadow-lg bg-white">
-      <Card/>
-      <Card/>
+    <div>
+      <Happiness country={country} />
     </div>
   )
 }
---
+++ client-side/src/pages/happiness.tsx
@@ -0,0 +1,48 @@
+import { useEffect, useState } from "react";
+import { API_URL } from "../constants/api";
+
+const groupByCountry = (happiness: any) => (
+  happiness.reduce((acc: any, row: any) => {
+    if (!acc[row.country]) {
+      acc[row.country] = [];
+    }
+    const { country, ...rest } = row;
+    acc[row.country].push(rest);
+    return acc;
+  }, {})
+)
+
+const Happiness = ({ country }: { country: string }): JSX.Element => {
+  const [happiness, setHappiness] = useState<any>({});
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await fetch(`${API_URL}/happiness?country=${country}`);
+        const json = await res.json();
+        setHappiness(groupByCountry(json));
+      } catch (error: any) {
+        console.log(error.message);
+      }
+    };
+    fetchData();
+  }, [country]);
+
+  return (
+    <div>
+      {Object.keys(happiness).map((country: string) => (
+        <div key={country}>
+        <h3>{country}</h3>
+        {happiness[country].map((row: any) => (
+          <div key={row.feature} className="flex flex-row">
+            <div>{row.feature}</div>
+            <div>{row.value} / {row.diff_rate}</div>
+          </div>
+        ))}
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export default Happiness;
