dd6114ff268da8199cccd08d8a2099ffded80dba
Yoichi Ojima <yoichiojima@gmail.com>
2024-12-18 22:37:33 +0900

    fix: use Task class in city_to_country.py

--- collection/collection/city_to_country.py
+++ collection/collection/city_to_country.py
@@ -1,26 +1,29 @@
 import json
 import pandas as pd
 from utils.utils import get_data_dir
+from task import Task
 
 
-def main():
-    output_path = get_data_dir() / "global/city_to_country.json"
+class CityToCountryTask(Task):
+    def extract(self):
+        output_path = get_data_dir() / "global/city_to_country.json"
+        return pd.read_json(get_data_dir() / "raw/cost_of_living.json")
 
-    df = pd.read_json(get_data_dir() / "raw/cost_of_living.json")
+    def transform(self, df: pd.DataFrame):
+        mapping_df = df[["country", "city"]].drop_duplicates()
+        mapping_df["country"] = mapping_df["country"].str.lower()
+        mapping_df["city"] = mapping_df["city"].str.lower()
+        return mapping_df
 
-    mapping_df = df[["country", "city"]].drop_duplicates()
+    def load(self, df: pd.DataFrame):
+        mapping = mapping_df.set_index("city").to_dict()["country"]
 
-    mapping_df["country"] = mapping_df["country"].str.lower()
-    mapping_df["city"] = mapping_df["city"].str.lower()
+        output_path.parent.mkdir(parents=True, exist_ok=True)
 
-    mapping = mapping_df.set_index("city").to_dict()["country"]
+        with open(output_path, "w") as f:
+            json.dump(mapping, f, indent=2)
 
-    output_path.parent.mkdir(parents = True, exist_ok = True)
-
-    with open(output_path, "w") as f:
-        json.dump(mapping, f, indent=2)
-
-    print(f"[city_to_country] saved: {output_path}")
+        print(f"[city_to_country] saved: {output_path}")
 
 
 if __name__ == "__main__":
