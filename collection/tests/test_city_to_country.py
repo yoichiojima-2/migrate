6eca94d9dba00b26759d6755840f77f43ae8b05a
Yoichi Ojima <yoichiojima@gmail.com>
2024-12-19 00:42:27 +0900

    chore: misc change

--- collection/tests/test_city_to_country.py
+++ collection/tests/test_city_to_country.py
@@ -1,8 +1,10 @@
 from pathlib import Path
 from collection.city_to_country import CityToCountryTask
+from collection.cost_of_living import CostOfLivingTask
 from utils.utils import get_data_dir
 
 def test_cost_of_living():
+    CostOfLivingTask().run()
     CityToCountryTask().run()
     assert Path(get_data_dir() / CityToCountryTask.output_path).exists()
 
