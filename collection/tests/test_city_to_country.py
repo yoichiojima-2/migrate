6f73759a7724552cc2544c8443faa1f256ad9d35
Yoichi Ojima <yoichiojima@gmail.com>
2024-12-21 10:45:07 +0900

    test: update test

--- collection/tests/test_city_to_country.py
+++ collection/tests/test_city_to_country.py
@@ -3,8 +3,8 @@ from collection.city_to_country import CityToCountryTask
 from collection.cost_of_living import CostOfLivingTask
 from utils.utils import get_data_dir
 
+
 def test_cost_of_living():
     CostOfLivingTask().run()
     CityToCountryTask().run()
     assert Path(get_data_dir() / CityToCountryTask.output_path).exists()
-
