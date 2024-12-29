3ade6826c5f8fd2fb69db4dc91af550502ce343e
Yoichi Ojima <yoichiojima@gmail.com>
2024-12-29 22:59:42 +0900

    feat: add happiness and QOL components

--- client-side/src/components/Card.tsx
+++ client-side/src/components/Card.tsx
@@ -7,8 +7,8 @@ interface CardProps {
 }
 
 const Card: React.FC<CardProps> = ({ city, happinessQOLData }) => (
-  <div>
-    <h2>{city}</h2>
+  <div className="m-4 p-4 border-2 rounded-2xl">
+    <h2 className="text-3xl p-4 mb-4">{city}</h2>
     <HappinessQOL city={city} data={happinessQOLData} />
   </div>
 );
--- client-side/src/components/HappinessQOL.tsx
+++ client-side/src/components/HappinessQOL.tsx
@@ -6,14 +6,36 @@ interface happinessQOLProps {
 }
 
 const HappinessQOL: React.FC<happinessQOLProps> = ({ city, data }) => {
+  const scale = 20;
   const filteredData = data.filter((row) => row.city === city);
+
+  filteredData.map((row) => {
+    console.log(row.diff * scale);
+  });
+
   return (
-    <div>
-      {filteredData.map((row) => (
-        <div key={row.feature}>
-          <p>
-            {row.feature}: {row.value} {row.value_in_current_city} {row.diff}
-          </p>
+    <div className="mb-4">
+      <h3 className="text-xl pb-3">compare with selected city</h3>
+      {filteredData.map((row, index) => (
+        <div key={index} className="flex justify-between h-5 text-sm">
+          <span className="w-5/12 text-right">{row.feature}</span>
+          <span className="relative w-1/12 text-right">{row.value}</span>
+          <span className="relative w-3/12 overflow-hidden">
+            <div
+              className="absolute right-0 bg-pink-500 h-4 rounded-l"
+              style={{
+                width: `${row.diff <= 0 ? Math.abs(row.diff * scale) : 0}%`,
+              }}
+            />
+          </span>
+          <span className="relative w-3/12 overflow-hidden">
+            <div
+              className="absolute left-0 bg-teal-500 h-4 rounded-r"
+              style={{
+                width: `${row.diff >= 0 ? row.diff * scale : 0}%`,
+              }}
+            />
+          </span>
         </div>
       ))}
     </div>
